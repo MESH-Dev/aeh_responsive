113d409fb783ea6d9d792f38b0469781111f45f7
karlshaver <karl@meshfresh.com>
2017-04-14 16:57:55 -0400

    Completes responsive header and mobile side menu function

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 $(function(){
    $('#loginButton').click(function(event){
       event.preventDefault();
+      if ($('#searchPanel').css('display') == 'block'){
+         $('#searchPanel').slideToggle();
+      };
       $('#loginBoxPanel').slideToggle();
+   });
+
+   $('#searchButton').click(function(){
+      if ($('#loginBoxPanel').css('display') == 'block'){
+         $('#loginBoxPanel').slideToggle();
+      };
+      $('#searchPanel').slideToggle();
+   });
+
+   $('#mobile-menu-trigger').sidr({
+      side: 'right'
    });
 });
